df1346d99461d7394881459bd8f69b6f8c26a741
iamdhavalparmar <dkp48107@gmail.com>
2021-04-19 13:47:21 +0530

    UI complete

---
+++ app/src/main/java/com/fundmanagement/Student/ChnagePassword.java
@@ -0,0 +1,14 @@
+package com.fundmanagement.Student;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChnagePassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chnage_password);
+    }
+}
